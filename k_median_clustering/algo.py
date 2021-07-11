82f8cf237adcd7b828604326ede07bb41947e764
Ron Visbord <ronvisbord@gmail.com>
2021-07-11 09:02:05 +0300

    improve edge cases and compete against KMeans

--- k_median_clustering/algo.py
+++ k_median_clustering/algo.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Iterable
 
 from k_median_clustering.math import *
@@ -16,6 +17,8 @@ class Reducer:
         removes from _Ni all points further than v from Ctmp.
         returns the number of remaining elements.
         """
+        if len(self.Ni) == 0:
+            return 0
         _, distances = pairwise_distances_argmin_min(self.Ni, Ctmp)
         remaining_points = distances > v
         self.Ni = self.Ni[remaining_points]
@@ -32,13 +35,16 @@ class Coordinator:
 
     def iterate(self, P1s: List[pd.DataFrame], P2s: List[pd.DataFrame], alpha) -> Tuple[float, pd.DataFrame]:
         P1 = pd.concat(P1s)
-        P2 = pd.concat(P2s) # TODO - do these copy the data? if so, avoid it
+        P2 = pd.concat(P2s)  # TODO - do these copy the data? if so, avoid it
 
         v, Ctmp = EstProc(P1, P2, alpha, self._dt, self._k, self._kp)
+        if v == 0.0:
+            logging.error("Bad! v == 0.0")
         self.C = pd.concat([self.C, Ctmp], ignore_index=True)
         return v, Ctmp
 
     def last_iteration(self, Nis: Iterable[pd.DataFrame]):
+        logging.info('starting last iteration...')
         N_remaining = pd.concat(Nis)
         Ctmp = A(N_remaining, self._k)
         self.C = pd.concat([self.C, Ctmp], ignore_index=True)
@@ -46,27 +52,39 @@ class Coordinator:
 
 def k_median_clustering(N: pd.DataFrame, k: int, ep: float, dt: float, m: int):
     n = len(N)
+    logging.info("starting to split")
     Ns = np.array_split(N, m)
+    logging.info("finished splitting")
     reducers = [Reducer(Ni) for Ni in Ns]
     kp = kplus_formula(k, dt)
     coordinator = Coordinator(k, kp, dt)
+    alpha = alpha_formula(n, k, ep, dt, len(N))
 
     remaining_elements_count = len(N)
     loop_count = 0
-    while remaining_elements_count > max_subset_size_formula(n, k, ep, dt):
-        alpha = alpha_formula(n, k, ep, dt, len(N))
+    max_subset_size = max_subset_size_formula(n, k, ep, dt)
+    logging.info(f"max_subset_size:{max_subset_size}")
+
+    while remaining_elements_count > 4 * max_subset_size or max_subset_size > 10 * r_formula(alpha, k, phi_alpha_formula(alpha, k, dt)):
+        logging.info(f"============ Starting LOOP {loop_count} ============")
         P1s_and_P2s = [r.sample_P1_P2(alpha) for r in reducers]
 
         P1s = [p1p2[0] for p1p2 in P1s_and_P2s]
-        P2s = [p1p2[1] for p1p2 in P1s_and_P2s] # TODO - test if this is wasteful for some reason
+        P2s = [p1p2[1] for p1p2 in P1s_and_P2s]
 
         v, Ctmp = coordinator.iterate(P1s, P2s, alpha)
 
         remaining_elements_count = sum(r.remove_handled_points(Ctmp, v) for r in reducers)
         loop_count += 1
+        alpha = alpha_formula(n, k, ep, dt, remaining_elements_count)
+        logging.info(f"============ END OF LOOP {loop_count}. "
+                     f"remaining_elements_count:{remaining_elements_count}."
+                     f" alpha:{alpha}. v:{v}. len(Ctmp):{len(Ctmp)}. "
+                     f" len(P2s):{sum(len(P2) for P2 in P2s)}. max_subset_size:{max_subset_size}"
+                     f" r:{r_formula(alpha, k, phi_alpha_formula(alpha, k, dt))}"
+                     f"  ============")
 
-    print(f'loop_count: {loop_count}')
+    coordinator.last_iteration([r.Ni for r in reducers])
 
-    coordinator.last_iteration(r.Ni for r in reducers)
-
+    logging.info(f'loop_count: {loop_count}. len(C):{len(coordinator.C)}')
     return coordinator.C
--- k_median_clustering/run_kmedoid_on_KDDCup1999.py
+++ k_median_clustering/run_kmedoid_on_KDDCup1999.py
@@ -1,32 +1,40 @@
 import numpy as np
 import pandas as pd
 from sklearn_extra.cluster import KMedoids
+from sklearn.cluster import KMeans
 
 from random import choice
 
 from k_median_clustering.algo import k_median_clustering
 from k_median_clustering.math import risk
 
+import logging
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
 DATASET_FILE = "/home/ronvis/private/distributed_k_median/data_samples/kddcup99/kddcup.data_10_percent_corrected.csv"
-SUBSET_SIZE = 5000
+SUBSET_SIZE = 600000
 full_data = pd.read_csv(DATASET_FILE, nrows=SUBSET_SIZE)
 N = full_data.select_dtypes([np.number])
 
-Ks = [20]#, 50, 100]
-Deltas = [0.1]#, 0.01]
-Ms = [100]#, 200]
-epsilons = [0.1]#, 0.15, 0.2]
+Ks = [5]  # , 50, 100]
+Deltas = [0.1]  # , 0.01]
+Ms = [50]  # , 200]
+epsilons = [0.2]  # , 0.15, 0.2]
 
 k = choice(Ks)
 dt = choice(Deltas)
 m = choice(Ms)
 ep = choice(epsilons)
 
-
 C = k_median_clustering(N, k, ep, dt, m)
+logging.info(f"Final size of C:{len(C)} x(and k:{k})")
 k_median_risk = risk(N, C)
-print(f'the k_median_clustering risk is {k_median_risk}')
+logging.info(f'The k_median_clustering risk is {k_median_risk}')
 
+# kmedoids = KMedoids(n_clusters=k).fit(N)
+# logging.info(f'The kmedoids.inertia is {kmedoids.inertia_}')
 
-kmedoids = KMedoids(n_clusters=k).fit(N)
-print(f'the kmedoids.inertia is {kmedoids.inertia_}')
+kmeans = KMeans(n_clusters=k, n_jobs=-1).fit(N)
+kmeans_risk = risk(N, kmeans.cluster_centers_)
+logging.info(f'The kmeans_risk is {kmeans_risk}')
