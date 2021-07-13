c09a807f25c8eafb4faa06500f096be7e5517ae4
Ron Visbord <ronvisbord@gmail.com>
2021-07-13 23:02:43 +0300

    implement a new competitor: scalable_k_means

--- k_median_clustering/run_kmedoid_on_KDDCup1999.py
+++ k_median_clustering/run_kmedoid_on_KDDCup1999.py
@@ -1,16 +1,15 @@
 import logging
 
-from k_median_clustering.algo import k_median_clustering
 from k_median_clustering import competitors
+from k_median_clustering.algo import k_median_clustering
 from k_median_clustering.math import risk, Blackbox
 import numpy as np
 import pandas as pd
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
-# DATASET_FILE = "/home/ronvis/private/distributed_k_median/data_samples/kddcup99/kddcup.data_10_percent_corrected.csv"
 DATASET_FILE = "/home/ronvis/private/distributed_k_median/data_samples/kddcup99/kddcup.data.corrected"
-SUBSET_SIZE = 60000000
+SUBSET_SIZE = 6000000
 full_data = pd.read_csv(DATASET_FILE, nrows=SUBSET_SIZE)
 N = full_data.select_dtypes([np.number])
 logging.info(f"Data size: {len(full_data):,}")
@@ -22,14 +21,25 @@ ep = 0.2
 
 logging.info(f"Starting distributed k median")
 C = k_median_clustering(N, k, ep, dt, m)
-logging.info(f"Final size of C:{len(C)} (where k:{k})")
 k_median_risk = risk(N, C)
-logging.info(f'The k_median_clustering risk is {k_median_risk:,}')
+logging.info(f'The k_median_clustering risk is {k_median_risk:,} and the size of C is {len(C)} (where k:{k}')
 
-logging.info(f"Starting {Blackbox.__name__}")
-blackbox_risk = competitors.blackbox(N, k, risk)
-logging.info(f'The {Blackbox.__name__} risk is {blackbox_risk:,}')
+iterations = 3
+l = int(1.2 * k)
 
-logging.info(f"Starting Spark KMeans")
-blackbox_risk = competitors.spark_kmeans(N, k)
-logging.info(f'The {Blackbox.__name__} risk is {blackbox_risk:,}')
+logging.info(f"Starting scalable_k_mean")
+scalable_k_means_C = competitors.scalable_k_means_clustering(N, iterations, l)
+scalable_k_means_risk = risk(N, scalable_k_means_C)
+logging.info(f'The scalable_k_means risk is {scalable_k_means_risk:,} and size of C is {len(scalable_k_means_C)}')
+
+# logging.info(f"Starting {Blackbox.__name__}")
+# blackbox_risk = competitors.blackbox(N, k, risk)
+# logging.info(f'The {Blackbox.__name__} risk is {blackbox_risk:,}')
+
+
+# try:
+#     logging.info(f"Starting Spark KMeans")
+#     blackbox_risk = competitors.spark_kmeans(N, k)
+#     logging.info(f'The {Blackbox.__name__} risk is {blackbox_risk:,}')
+# except Exception as e:
+#     logging.error(e)
