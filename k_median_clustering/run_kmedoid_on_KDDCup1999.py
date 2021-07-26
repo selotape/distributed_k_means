d1700d0bc233013f0dced2c2ec721cf98fc787e6
ronvis <ronvis@google.com>
2021-07-26 09:11:02 +0300

    improve logging. add git-label. add TODOs. run everything 4 times. calculate avg risks. start calculating avg machine-time

--- k_median_clustering/run_kmedoid_on_KDDCup1999.py
+++ k_median_clustering/run_kmedoid_on_KDDCup1999.py
@@ -1,16 +1,22 @@
+from collections import defaultdict
 from itertools import product
 from time import strftime
-
+from statistics import mean
 from k_median_clustering import competitors
 from k_median_clustering.algo import distributed_k_median_clustering
 from k_median_clustering.math import risk
 from k_median_clustering.utils import setup_logger
 import numpy as np
 import pandas as pd
+import subprocess
+
 
 log_time = strftime('%Y%m%d%H%M')
 logger = setup_logger('full_log', f'k_median_clustering_log_{log_time}.log', with_console=True)
-results = setup_logger('results_log', f'k_median_clustering_results_{log_time}.log')
+results = setup_logger('results_log', f'k_median_clustering_results_{log_time}.log', another_file=f'k_median_clustering_log_{log_time}.log' ,with_console=True)
+
+label = subprocess.check_output(["git", "describe"]).strip()
+results.info(f"Running experiment with git label {label}")
 
 DATASET_FILE = "/home/ronvis/private/distributed_k_median/data_samples/kddcup99/kddcup.data.corrected"
 SUBSET_SIZE = 6000000
@@ -22,38 +28,59 @@ ks = [10, 50, 100, 500]
 epsilons = [0.15, 0.2]
 deltas = [0.1]
 ms = [50]
+more = [1, 2, 3, 4]
 
+
+# 1. avg of ratio of risks & risk_f
+# ... future: variance of ratios
+# 2. sum/avg_iters(max(sample_phase) + bb_phase + max(trim_phase) + )
+# 3. make skm ""distributed"". measure sum+avg like above
+# 4. new data set
 
 def main():
-    for k, dt, m, ep in product(ks, deltas, ms, epsilons):
-        logger.info(f'=============================================================================================')
-        logger.info(f"======== Starting distributed k median with len(N)={len(N)} k={k} dt={dt} ep={ep} & m={m} ========")
-        logger.info(f'=============================================================================================')
-        results.info('Starting...\n')
-        dkm_C, dkm_C_final, dkm_iters = distributed_k_median_clustering(N, k, ep, dt, m)
-        dkm_risk = risk(N, dkm_C)
-        dkm_risk_f = risk(N, dkm_C_final)
-        logger.info(f'=============================================================================================')
-        logger.info(f'=============================================================================================')
-        logger.info(f'=============================================================================================')
-        results.info(f'len(N):{len(N)}. dkm_risk:{dkm_risk:,}. dkm_risk_final:{dkm_risk_f:,}. len(dkm_C):{len(dkm_C)}. len(dkm_C_final):{len(dkm_C_final)}')
+    skm_runs = 4
+    risks = defaultdict(list)
+    for k, dt, m, ep, _ in product(ks, deltas, ms, epsilons, more):
+        try:
+            logger.info(f'=============================================================================================')
+            logger.info(f"======== Starting distributed k median with len(N)={len(N)} k={k} dt={dt} ep={ep} & m={m} ========")
+            logger.info(f'=============================================================================================')
+            results.info('Starting...\n')
+            dkm_C, dkm_C_final, dkm_iters = distributed_k_median_clustering(N, k, ep, dt, m, logger, results)
+            dkm_risk = risk(N, dkm_C)
+            dkm_risk_f = risk(N, dkm_C_final)
 
-        l = int(len(dkm_C) / dkm_iters)
+            risks['dkm'].append(dkm_risk), risks['dkm_f'].append(dkm_risk_f)
 
-        for i in range(4):
-            skm_iters = dkm_iters + i
-            logger.info(f"{i}. Starting scalable_k_mean with {skm_iters} iterations and l=={l}")
-            skm_C, skm_C_final = competitors.scalable_k_means_clustering(N, skm_iters, l, k)
-            skm_risk = risk(N, skm_C)
-            skm_risk_f = risk(N, skm_C_final)
-            logger.info(f'The scalable_k_means risk is {skm_risk:,} and size of C is {len(skm_C)}')
-            logger.info(f'=============================================================================================')
-            test_summary = summarize(i, 'skm 1', k, dt, m, ep, len(dkm_C), dkm_iters, skm_iters, l, len(skm_C), dkm_risk, skm_risk, dkm_risk_f, skm_risk_f)
-            logger.info(test_summary)
-            results.info(test_summary)
             logger.info(f'=============================================================================================')
             logger.info(f'=============================================================================================')
             logger.info(f'=============================================================================================')
+            results.info(f'len(N):{len(N)}. dkm_risk:{dkm_risk:,}. dkm_risk_final:{dkm_risk_f:,}. len(dkm_C):{len(dkm_C)}. len(dkm_C_final):{len(dkm_C_final)}')
+
+            l = int(len(dkm_C) / dkm_iters)
+
+
+            for i in range(skm_runs):
+                skm_iters = dkm_iters + i
+                logger.info(f"{i}. Starting scalable_k_mean with {skm_iters} iterations and l=={l}")
+                skm_C, skm_C_final = competitors.scalable_k_means_clustering(N, skm_iters, l, k)
+                skm_risk = risk(N, skm_C)
+                skm_risk_f = risk(N, skm_C_final)
+                risks[f'skm_{i}'].append(skm_risk), risks[f'skm_f_{i}'].append(skm_risk_f)
+                logger.info(f'The scalable_k_means risk is {skm_risk:,} and size of C is {len(skm_C)}')
+                logger.info(f'=============================================================================================')
+                test_summary = summarize(i, 'skm 1', k, dt, m, ep, len(dkm_C), dkm_iters, skm_iters, l, len(skm_C), dkm_risk, skm_risk, dkm_risk_f, skm_risk_f)
+                results.info(test_summary)
+                logger.info(f'=============================================================================================')
+                logger.info(f'=============================================================================================')
+                logger.info(f'=============================================================================================')
+        except Exception as e:
+            results.error("BAD!" + str(e))
+
+
+        risk_avg_str = ', '.join(f'dkm_r/smk_{i}_r' + str(mean(risks['dkm']) / mean(risks[f'skm_{i}']) for i in range(skm_runs)))
+        risk_f_avg_str = ', '.join(f'dkm_f_r/smk_{i}_f_r' + str(mean(risks['dkm_f']) / mean(risks[f'skm_f_{i}']) for i in range(skm_runs)))
+
 
         #
         # logger.info(f'=============================================================================================')
--- k_median_clustering/utils.py
+++ k_median_clustering/utils.py
@@ -3,12 +3,10 @@ import sys
 
 import time
 
-
-
 formatter = logging.Formatter('%(asctime)s %(message)s')
 
 
-def setup_logger(name, log_file, with_console=False, level=logging.INFO):
+def setup_logger(name, log_file, another_file=None, with_console=False, level=logging.INFO):
     """To setup as many loggers as you want"""
 
     file_handler = logging.FileHandler(log_file)
@@ -18,14 +16,21 @@ def setup_logger(name, log_file, with_console=False, level=logging.INFO):
     logger.setLevel(level)
     logger.addHandler(file_handler)
 
+    if another_file:
+        another_file_handler = logging.FileHandler(another_file)
+        another_file_handler.setFormatter(formatter)
+        logger.addHandler(another_file_handler)
+
     if with_console:
         console = logging.StreamHandler(sys.stdout)
+        console.setFormatter(formatter)
         logger.addHandler(console)
 
     return logger
 
 
 LAST_RUNTIME = '_last_runtime'
+
 
 def keep_time(func):
     '''Instance-Method decorator that saves the methods last execution time on the instance.'''
@@ -39,3 +44,21 @@ def keep_time(func):
         return result
 
     return wrap
+
+
+# At the beginning of every .py file in the project
+def logger(fn):
+    from functools import wraps
+    import inspect
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        log = logging.getLogger(fn.__name__)
+        log.info('About to run %s' % fn.__name__)
+
+        out = fn(*args, **kwargs)
+
+        log.info('Done running %s' % fn.__name__)
+        # Return the return value
+        return out
+
+    return wrapper
