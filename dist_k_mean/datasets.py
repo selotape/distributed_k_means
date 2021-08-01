4744e9b9117b71d35df9e66bb920e054bb9435e7
Ron Visbord <ronvisbord@gmail.com>
2021-08-01 23:21:17 +0300

    make more stuff configurable and prep for tonights runs

--- dist_k_mean/datasets.py
+++ dist_k_mean/datasets.py
@@ -1,23 +1,35 @@
+from functools import lru_cache
+
 import pandas as pd
 
 from math import floor
 
 import numpy as np
 
+from config import *
+
+
+@lru_cache
+def get_dataset(dataset):
+    if dataset == 'kdd':
+        return read_and_prep_kdd()
+    elif dataset == 'gaussian':
+        return generate_k_gaussians()
+    else:
+        raise RuntimeError(f"bad dataset {dataset}")
+
 
 def read_and_prep_kdd():
-    DATASET_FILE = "/home/ronvis/private/distributed_k_median/data_samples/kddcup99/kddcup.data.corrected"
-    SUBSET_SIZE = 6000000
-    full_data = pd.read_csv(DATASET_FILE, nrows=SUBSET_SIZE)
+    full_data = pd.read_csv(KDD_DATASET_FILE, nrows=KDD_SUBSET_SIZE)
     N = full_data.select_dtypes([np.number])
     return N
 
 
-def generate_k_gaussians(dimensions=5, num_points=10_000_000, K=50, alpha=0.0, std_dev=0.1):
-    N = np.random.normal(scale=std_dev, size=(num_points, dimensions,))
-    centers = np.random.uniform(size=(K, dimensions,))
-    the_sum = sum(i ** alpha for i in range(1, K + 1))
-    cluster_sizes = [floor(num_points * ((i ** alpha) / the_sum)) for i in range(1, K + 1)]
+def generate_k_gaussians():
+    N = np.random.normal(scale=GAUSSIANS_STD_DEV, size=(GAUSSIANS_NUM_POINTS, GAUSSIANS_DIMENSIONS,))
+    centers = np.random.uniform(size=(GAUSSIANS_K, GAUSSIANS_DIMENSIONS,))
+    the_sum = sum(i ** GAUSSIANS_ALPHA for i in range(1, GAUSSIANS_K + 1))
+    cluster_sizes = [floor(GAUSSIANS_NUM_POINTS * ((i ** GAUSSIANS_ALPHA) / the_sum)) for i in range(1, GAUSSIANS_K + 1)]
     N = N[:sum(cluster_sizes)]
 
     cluster_slices = []
