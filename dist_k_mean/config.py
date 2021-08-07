12da2ce1719a3eff8633a0facdeced65ab142a11
ronvis <ronvis@google.com>
2021-08-07 23:55:52 +0300

    reimplement config for cli override. Create main run_script
    also add useful_commands

--- dist_k_mean/config.py
+++ dist_k_mean/config.py
@@ -1,33 +1,36 @@
-RUN_NAME = 'SCHMOD'
+import os
+import sys
+
+RUN_NAME = 'SCHMOD' if len(sys.argv) < 2 else sys.argv[1]
 
 ###### DATA SETS ######
-DATASETS = ['gaussian', 'kdd', ]  # 'gaussian', 'kdd'
+DATASET = os.getenv('DATASET', default='gaussian')  # 'gaussian', 'kdd'
 
-KDD_DATASET_FILE = "data_samples/kddcup99/kddcup.data"
-KDD_SUBSET_SIZE = 6000000
+KDD_DATASET_FILE = os.getenv('KDD_DATASET_FILE', default="data_samples/kddcup99/kddcup.data")  # const
+KDD_SUBSET_SIZE = int(os.getenv('KDD_SUBSET_SIZE', default=6_000_000))
 
-GAUSSIANS_DIMENSIONS = 10
-GAUSSIANS_NUM_POINTS = 10_000_000
-GAUSSIANS_K = 50
-GAUSSIANS_ALPHA = 0.0
-GAUSSIANS_STD_DEV = 0.1
+GAUSSIANS_DIMENSIONS = int(os.getenv('GAUSSIANS_DIMENSIONS', default=15))
+GAUSSIANS_NUM_POINTS = int(os.getenv('GAUSSIANS_NUM_POINTS', default=10_000_000))
+GAUSSIANS_K = int(os.getenv('GAUSSIANS_K', default=100))
+GAUSSIANS_ALPHA = float(os.getenv('GAUSSIANS_ALPHA', default=0.0))
+GAUSSIANS_STD_DEV = float(os.getenv('GAUSSIANS_STD_DEV', default=0.1))
 
 ###### BLACK_BOXES ######
-INNER_BLACKBOX = 'ScalableKMeans'  # 'KMeans' 'MiniBatchKMeans' 'ScalableKMeans'
-INNER_BLACKBOX_ITERATIONS = 3
-INNER_BLACKBOX_L_TO_K_RATIO = 1
+INNER_BLACKBOX = os.getenv('INNER_BLACKBOX', default='KMeans')  # 'KMeans' 'MiniBatchKMeans' 'ScalableKMeans'
+INNER_BLACKBOX_ITERATIONS = int(os.getenv('INNER_BLACKBOX_ITERATIONS', default=4))
+INNER_BLACKBOX_L_TO_K_RATIO = int(os.getenv('INNER_BLACKBOX_L_TO_K_RATIO', default=1))
 
-FINALIZATION_BLACKBOX = 'KMeans'
+FINALIZATION_BLACKBOX = os.getenv('FINALIZATION_BLACKBOX', default='KMeans')
 
-MINI_BATCH_SIZE = 1000
+MINI_BATCH_SIZE = int(os.getenv('MINI_BATCH_SIZE', default=1000))
 
 ###### DISTRIBUTED PARAMS ######
-ROUNDS = 3
-KS = [50]
-EPSILONS = [0.1]
+ROUNDS = int(os.getenv('ROUNDS', default=5))
+KS = [25, 50, 100]
+EPSILONS = [0.1, 0.2]
 DELTAS = [0.1]
 MS = [50]
 
 ###### SKM PARAMS ######
-L_TO_K_RATIOS = [1]
-SKM_ITERATIONS = [2, 3]
+L_TO_K_RATIOS = [1, 2, 5]
+SKM_ITERATIONS = [2, 3, 4, 5]
