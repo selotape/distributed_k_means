1a6932610bf0f859ae2d7f39b2f9c855bea74826
ronvis <ronvis@google.com>
2021-08-12 20:49:58 +0300

    update consts. create strict & fast modes.

--- dist_k_mean/math.py
+++ dist_k_mean/math.py
@@ -5,12 +5,14 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import pairwise_distances_argmin_min, pairwise_distances_argmin
 
+from dist_k_mean.config import CONST_MODE, PHI_ALPHA, MAX_SS_SIZE, KPLUS
 
-def kplus_formula(k: int, dt: float):
+
+def kplus_formula(k: int, dt: float, ep: float):
     """
     The allowed size of the "k+" clusters group
     """
-    return int(k + 8 * log(1.1 * k / dt))
+    return int(k + KPLUS[CONST_MODE] * log(1.1 * k / (dt * ep)))
 
 
 def max_subset_size_formula(n: int, k: int, ep: float, dt: float):
@@ -18,7 +20,7 @@ def max_subset_size_formula(n: int, k: int, ep: float, dt: float):
     The size above which data doesn't fit inside a single machine,
     so clustering must be distributed.
     """
-    return 16 * k * pow(n, ep) * log(1.1 * k / dt)
+    return MAX_SS_SIZE[CONST_MODE] * k * pow(n, ep) * log(1.1 * k / dt)
 
 
 def alpha_formula(n, k, ep, dt, N_current_size):
@@ -28,7 +30,6 @@ def alpha_formula(n, k, ep, dt, N_current_size):
     10k n^ep log(8k/dt) / |N|
     """
     return max_subset_size_formula(n, k, ep, dt) / N_current_size
-
 
 
 def risk_kmeans(N: pd.DataFrame, C: pd.DataFrame):
@@ -57,7 +58,7 @@ def phi_alpha_formula(alpha: float, k: int, dt: float, ep: float):
     """
     The size of the already-handled clusters
     """
-    return (5 / alpha) * log(1.1 * k / (dt * ep))
+    return (PHI_ALPHA[CONST_MODE] / alpha) * log(1.1 * k / (dt * ep))
 
 
 def r_formula(alpha: float, k: int, phi_alpha: float) -> int:
@@ -66,7 +67,6 @@ def r_formula(alpha: float, k: int, phi_alpha: float) -> int:
 
 def v_formula(psi: float, k: int, phi_alpha: float):
     return psi / (k * phi_alpha)
-
 
 
 def pairwise_distances_argmin_min_squared(X, Y):
