c582672ebf9192b6fb96f231be97b4838c4dc1e7
Ron Visbord <ronvisbord@gmail.com>
2021-08-18 22:19:16 +0300

    improvements to run_exp.sh

--- run_meta_experiment.sh
+++ run_meta_experiment.sh
@@ -10,7 +10,6 @@ ALGO=SKM SKM_ITERATIONS=3 run_exp "${DATASET}_${K}K_skm_3iters"
 ALGO=SKM SKM_ITERATIONS=4 run_exp "${DATASET}_${K}K_skm_4iters"
 ALGO=SKM SKM_ITERATIONS=5 run_exp "${DATASET}_${K}K_skm_5iters"
 
-ALGO=DKM INNER_BLACKBOX=KMeans CONST_MODE=strict run_exp "${DATASET}_${K}K_dkm_strict_KMBlackBox"
-ALGO=DKM INNER_BLACKBOX=ScalableKMeans CONST_MODE=strict run_exp "${DATASET}_${K}K_dkm_strict_SKMBlackBox"
-ALGO=DKM INNER_BLACKBOX=KMeans CONST_MODE=fast run_exp "${DATASET}_${K}K_dkm_fast_KMBlackBox"
-ALGO=DKM INNER_BLACKBOX=ScalableKMeans CONST_MODE=fast run_exp "${DATASET}_${K}K_dkm_fast_SKMBlackBox"
+ALGO=DKM EPSILON=0.2 run_exp "${DATASET}_${K}K_dkm"
+
+ALGO=ENE EPSILON=0.2 run_exp "${DATASET}_${K}K_ene"
