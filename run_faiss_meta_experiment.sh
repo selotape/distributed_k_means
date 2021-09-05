37c258e92b59b58c8e02c81c146bd7afe904be5a
Ron Visbord <ronvisbord@gmail.com>
2021-09-05 21:37:55 +0300

    deactivate soccer conda env

--- run_faiss_meta_experiment.sh
+++ run_faiss_meta_experiment.sh
@@ -8,18 +8,18 @@ function run_exp() {
 TIMESTAMP="$(date +'%m-%d-%H-%M')"
 export TIMESTAMP
 
-__conda_setup="$('/home/anaconda3.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/anaconda3.8/etc/profile.d/conda.sh" ]; then
-        . "/home/anaconda3.8/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/anaconda3.8/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-conda activate soccer
+#__conda_setup="$('/home/anaconda3.8/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#    eval "$__conda_setup"
+#else
+#    if [ -f "/home/anaconda3.8/etc/profile.d/conda.sh" ]; then
+#        . "/home/anaconda3.8/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/home/anaconda3.8/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
+#conda activate soccer
 
 ALGO=SKM SKM_ITERATIONS=1 run_exp "${DATASET}_${K}K_skm_1iters"
 #ALGO=SKM SKM_ITERATIONS=2 run_exp "${DATASET}_${K}K_skm_2iters"
