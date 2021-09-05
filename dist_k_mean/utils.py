599577f8d768c18953da3f1efea14b3108346d9c
ronvis <ronvis@google.com>
2021-09-05 21:28:21 +0300

    support envs without faiss installed

--- dist_k_mean/utils.py
+++ dist_k_mean/utils.py
@@ -123,3 +123,12 @@ class SimpleMeasurement(Measurement):
 
     def iterations(self):
         return self.iterations_
+
+
+def module_exists(module_name):
+    try:
+        __import__(module_name)
+    except ImportError:
+        return False
+    else:
+        return True
