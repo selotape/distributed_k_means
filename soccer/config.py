feb786aff3701a2967fb3db95b687216a847bc2d
ronvis <ronvis@google.com>
2021-09-07 23:05:47 +0300

    remove unused gaussing dataset type

--- soccer/config.py
+++ soccer/config.py
@@ -30,7 +30,6 @@ SCALE_DATASET = bool(os.getenv('SCALE_DATASET', default=False))
 ###### Gaussian dataset ######
 GAUSSIANS_DIMENSIONS = int(os.getenv('GAUSSIANS_DIMENSIONS', default=15))
 GAUSSIANS_K = int(os.getenv('GAUSSIANS_K', default=100))
-GAUSSIANS_TYPE = os.getenv('GAUSSIANS_TYPE', default='gamma')  # 'exp' 'gamma'
 GAUSSIANS_GAMMA = float(os.getenv('GAUSSIANS_GAMMA', default=1.5))
 GAUSSIANS_STD_DEV = float(os.getenv('GAUSSIANS_STD_DEV', default=0.001))
 GAUSSIANS_LOW = float(os.getenv('GAUSSIANS_LOW', default=0.0))
