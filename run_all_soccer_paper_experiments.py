607137e9fbc12c29bb7463b90fb0c0e8c681f3a6
Ron Visbord <ronvisbord@gmail.com>
2021-12-29 11:22:04 +0200

    add kp scaler to filename

--- run_all_soccer_paper_experiments.py
+++ run_all_soccer_paper_experiments.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import os
 import subprocess
 from itertools import product
 
@@ -24,7 +24,7 @@ def run_meta_experiment(dataset, k):
 
 
 def run_soccer(k, dataset, epsilon):
-    subprocess.call(f"K={k} DATASET={dataset} INNER_BLACKBOX=FaissKMeans EPSILON={epsilon} ./run_a_soccer_experiment.py {dataset}_{k}K_{epsilon}ep_soccer", shell=True)
+    subprocess.call(f"K={k} DATASET={dataset} INNER_BLACKBOX=FaissKMeans EPSILON={epsilon} ./run_a_soccer_experiment.py {dataset}_{k}K_{os.getenv('KP_SCALER')}kpscale_{epsilon}ep_soccer", shell=True)
 
 
 def run_skm(k, dataset, skm_iters):
