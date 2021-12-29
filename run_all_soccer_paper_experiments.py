03e48909d1df4ac94eca0406c6caa875e190cc35
ronvis <ronvis@google.com>
2021-12-29 08:48:54 +0200

    Revert "extremely reduce "run_all_experients" to test setups"
    This reverts commit e264770beb8e2d5beef9d3b00d1d811deb08e21b.

--- run_all_soccer_paper_experiments.py
+++ run_all_soccer_paper_experiments.py
@@ -5,21 +5,21 @@ from itertools import product
 
 
 def main():
-    gaussian_experiments = (("gaussian_200", 200))
+    gaussian_experiments = (("gaussian_25", 25), ("gaussian_50", 50), ("gaussian_100", 100), ("gaussian_200", 200))
     for dataset, k in gaussian_experiments:
         run_meta_experiment(dataset, k)
 
-    # other_experiments = product(("higgs", "kdd", "census", "bigcross"), (25, 50, 100, 200))
-    # for dataset, k in other_experiments:
-    #     run_meta_experiment(dataset, k)
+    other_experiments = product(("higgs", "kdd", "census", "bigcross"), (25, 50, 100, 200))
+    for dataset, k in other_experiments:
+        run_meta_experiment(dataset, k)
 
 
 def run_meta_experiment(dataset, k):
-    for epsilon in (0.1, ):
+    for epsilon in (0.01, 0.05, 0.1, 0.2,):
         if (k, epsilon) == (200, 0.2):
             continue
         run_soccer(k, dataset, epsilon)
-    for skm_iters in ( 3,):
+    for skm_iters in (1, 2, 3, 4, 5):
         run_skm(k, dataset, skm_iters)
 
 
