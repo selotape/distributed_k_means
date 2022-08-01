3ed7427f326e4d371f4fcdb0ba792254c90acdd6
Ron Visbord <ronvisbord@gmail.com>
2022-08-01 14:25:24 +0300

    run demo on KDDCup Data

--- spark/src/main/scala/org/ronvis/soccer/demo/SoccerDemo.scala
+++ spark/src/main/scala/org/ronvis/soccer/demo/SoccerDemo.scala
@@ -2,7 +2,8 @@ package org.ronvis.soccer.demo
 
 import org.apache.spark.ml.clustering.MyKMeans
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.ml.feature.VectorAssembler
 
 object SoccerDemo {
   def main(args: Array[String]): Unit = {
@@ -13,10 +14,11 @@ object SoccerDemo {
       .appName("SOCCER example")
       .getOrCreate()
 
-    val dataset = spark.read.format("libsvm").load("../datasets/sample_kmeans_data.txt")
+    //    val dataset = spark.read.format("libsvm").load("../datasets/sample_kmeans_data.txt")
+    val dataset = getKddCup99Dataset(spark)
 
-    // Trains a k-means model.
-    val kmeans = new MyKMeans().setK(2).setSeed(1L)
+    //     Trains a k-means model.
+    val kmeans = new MyKMeans().setK(10).setSeed(1L)
     val model = kmeans.fit(dataset)
 
     // Make predictions
@@ -32,5 +34,16 @@ object SoccerDemo {
     println("Cluster Centers: ")
     model.clusterCenters.foreach(println)
 
+  }
+
+  def getSampleKMeansDataset(spark: SparkSession): Dataset[_] = {
+    spark.read.format("libsvm").load("../datasets/sample_kmeans_data.txt")
+  }
+
+  def getKddCup99Dataset(spark: SparkSession): Dataset[_] = {
+    // TODO - change to "drop all none numeric columns"
+    val frame = spark.read.format("csv").option("inferSchema", "true").load("../datasets/kddcup99/kddcup.data").drop("_c1", "_c2", "_c3", "_c41")
+    val assembler = new VectorAssembler().setInputCols(frame.columns).setOutputCol("features")
+    assembler.transform(frame)
   }
 }
