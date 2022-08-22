385b18466de774910e8353a6aceb0560503ae401
Ron Visbord <ronvisbord@gmail.com>
2022-08-22 22:19:25 +0300

    function to remove all nonnumeric columns

--- spark/src/main/scala/org/ronvis/soccer/demo/SoccerDemo.scala
+++ spark/src/main/scala/org/ronvis/soccer/demo/SoccerDemo.scala
@@ -1,7 +1,7 @@
 package org.ronvis.soccer.demo
 
 import org.apache.logging.log4j.{LogManager, Logger}
-import org.apache.spark.ml.clustering.SoccerKMeans
+import org.apache.spark.ml.clustering.{KMeans, SoccerKMeans}
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.{Estimator, Model}
@@ -21,23 +21,24 @@ object SoccerDemo {
 
     //    val dataset = getSampleKMeansDataset(spark)
     val dataset = getKddCup99Dataset(spark)
-
     val seed = 1L
-    val k = 10
+    val k = 25
+
+    log.info("================== STARTING SOCCER KMEANS ==================")
     val soccerKmeans = new SoccerKMeans()
       .setK(k)
-      .setM(10)
-      .setTol(0.1) // aka - epsilon
+      .setM(50)
+      .setTol(0.05) // aka - epsilon
       .setDelta(0.1)
       .setSeed(seed)
       .setMaxIter(3)
-    log.info("================== STARTING SOCCER KMEANS ==================")
-    fitAndEvaluate(soccerKmeans, dataset)
+    //    fitAndEvaluate(soccerKmeans, dataset)
     log.info("================== FINISHED SOCCER KMEANS ==================")
 
-//    log.info("================== STARTING LEGACY KMEANS ==================")
-    //    val boringOldschoolKmeans = new KMeans().setK(k).setSeed(seed)
+    log.info("================== STARTING LEGACY KMEANS ==================")
+    val boringOldschoolKmeans = new KMeans().setK(k).setSeed(seed)
     //    fitAndEvaluate(boringOldschoolKmeans, dataset)
+    log.info("================== FINISHED LEGACY KMEANS ==================")
   }
 
   def fitAndEvaluate(kmeans: Estimator[_ <: Model[_]], dataset: DataFrame): Unit = {
@@ -53,9 +54,32 @@ object SoccerDemo {
   }
 
   def getKddCup99Dataset(spark: SparkSession): DataFrame = {
-    // TODO - change to "drop all none numeric columns"
-    val frame = spark.read.format("csv").option("inferSchema", "true").load("../datasets/kddcup99/kddcup.data").drop("_c1", "_c2", "_c3", "_c41").limit(10000)
-    val assembler = new VectorAssembler().setInputCols(frame.columns).setOutputCol("features")
-    assembler.transform(frame)
+    val frame = spark.read.format("csv").option("inferSchema", "true").load("../datasets/kddcup99/kddcup.data")
+    val numericFrame = retainNumericColumnsAndLog(frame)
+    val assembler = new VectorAssembler().setInputCols(numericFrame.columns).setOutputCol("features")
+    assembler.transform(numericFrame)
   }
+
+  def getKddCup99DatasetTop10k(spark: SparkSession): DataFrame = {
+    getKddCup99Dataset(spark).limit(10000)
+  }
+
+  private def retainNumericColumnsAndLog(df: DataFrame): DataFrame = {
+    printColumnTypes("Before cleanup: ", df)
+    val cleanedDf = removeNonNumericColumns(df)
+    printColumnTypes(" After cleanup: ", cleanedDf)
+    cleanedDf
+  }
+
+  private val NON_NUMERIC_COLUM_TYPES = Set("string", "boolean")
+
+  private def removeNonNumericColumns(df: DataFrame): DataFrame = {
+    val numericColumns = df.columns.filter(cName => !NON_NUMERIC_COLUM_TYPES.contains(df.schema(cName).dataType.typeName))
+    df.select(numericColumns(0), numericColumns: _*)
+  }
+
+  private def printColumnTypes(prefix: String, df: DataFrame): Unit = {
+    log.info(prefix + df.columns.map(c => df.schema(c).dataType.typeName).mkString("Array(", ", ", ")"))
+  }
+
 }
